3305b4dfc2e8d0d5acc1b936132e18caa59e5587
nelson6868 <ekwomnelson090@gmail.com>
2022-12-05 14:20:26 +0100

    Add project files.

---
+++ ConsoleTimerApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleTimerApp;
+using System.Diagnostics;
+namespace Time_Alert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a Worktime duration in minutes: ");
+                int WorkDuration = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter a Resttime duration in minutes: ");
+                int RestDuration = Convert.ToInt32(Console.ReadLine());
+                startTimer st = new startTimer();
+                st.consoleTimerCount(WorkDuration, RestDuration);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error in input format, please try again");
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ ConsoleTimerApp/startTimer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+namespace ConsoleTimerApp
+{
+
+    public class startTimer
+    {
+        public bool EndTimerCount = true;
+        public void consoleTimerCount(int WorkDuration, int RestDuration)
+        {
+
+            DateTime StartTime = DateTime.Now;
+            while (EndTimerCount)
+            {
+                Stopwatch WorkStopwatch = new();
+                if (WorkDuration > 0)
+                {
+                    int WorkTimeInMinutes = WorkDuration * 1000 * 60;
+                    WorkStopwatch.Start();
+                    Console.WriteLine("Work Timer Active...");
+                    Thread.Sleep(WorkTimeInMinutes);
+                    WorkStopwatch.Stop();
+                }
+                else
+                {
+                    Console.WriteLine("Worktime you entered is invalid");
+                }
+                TimeSpan workTimeStop = WorkStopwatch.Elapsed;
+                Console.WriteLine("Your WorkTime has completed");
+                Console.WriteLine("\n");
+                Stopwatch RestStopwatch = new();
+                if (RestDuration > 0)
+                {
+                    int RestTimeInMinutes = RestDuration * 1000 * 60;
+                    RestStopwatch.Start();
+                    Console.WriteLine("Rest Timer Active...");
+                    Thread.Sleep(RestTimeInMinutes);
+                    RestStopwatch.Stop();
+                }
+                else
+                {
+                    Console.WriteLine("The RestTime you entered is invalid");
+                }
+                TimeSpan RestTimeStop = RestStopwatch.Elapsed;
+                Console.WriteLine("\n");
+                Console.WriteLine("Your RestTime Have Ended");
+                Console.WriteLine("Do you want to continue this cycle? (y/n) ");
+                string CheckTimer = Console.ReadLine();
+                if (CheckTimer == "y")
+                {
+                    EndTimerCount = true;
+                }
+                else if (CheckTimer == "n")
+                {
+                    EndTimerCount = false;
+                }
+                else
+                {
+                    EndTimerCount = true;
+                }
+            }
+            DateTime EndTime = DateTime.Now;
+            Console.WriteLine("Your session has ended: ");
+            Console.WriteLine($"Start Time: {StartTime.ToShortTimeString()}, \n  End Time: {EndTime.ToShortTimeString()}.");
+
+
+
+
+
+
+        }
+    }
+
+
+
+}
